73bc1a86f3c1cab02760db9791d9f7746f7b697e
olhacodes <olha.s2018@gmail.com>
2022-11-22 15:57:28 +0200

    added blocks, mixins, card, carousel, icons, button components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,37 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { TableComponent } from './components/table/table.component';
-import { RadioButtonComponent } from './components/radio-button/radio-button.component';
+import { RadioButtonComponent } from './components/icons/radio-button/radio-button.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardComponent } from './components/card/card.component';
+import { LastViewedBlockComponent } from './blocks/last-viewed-block/last-viewed-block.component';
+import { TableBlockComponent } from './blocks/table-block/table-block.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { IconStarComponent } from './components/icons/icon-star/icon-star.component';
+import { FlagIconComponent } from './components/icons/flag-icon/flag-icon.component';
+import { ButtonComponent } from './components/button/button.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DropdownComponent,
     TableComponent,
-    RadioButtonComponent
+    RadioButtonComponent,
+    CarouselComponent,
+    CardComponent,
+    LastViewedBlockComponent,
+    TableBlockComponent,
+    CardListComponent,
+    IconStarComponent,
+    FlagIconComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +42,8 @@ import { RadioButtonComponent } from './components/radio-button/radio-button.com
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    RouterModule
+    RouterModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/blocks/last-viewed-block/last-viewed-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last-viewed-block',
+  templateUrl: './last-viewed-block.component.html',
+  styleUrls: ['./last-viewed-block.component.scss']
+})
+export class LastViewedBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
