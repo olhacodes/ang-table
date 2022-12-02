2d11f91bb2df3193add50f941d3dcdcf589de705
olhacodes <olha.s2018@gmail.com>
2022-12-02 15:36:01 +0200

    added sidebar

--- src/app/blocks/table-block/table-block.component.ts
+++ src/app/blocks/table-block/table-block.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table-block',
   templateUrl: './table-block.component.html',
-  styleUrls: []
+  styleUrls: ['./table-block.component.scss']
 })
 export class TableBlockComponent implements OnInit {
 
